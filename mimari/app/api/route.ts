4d4b53d3d5ce25c108670b04220a28bb2e26198a
Cigerxwin İslam <islamtunc748@gmail.com>
2024-07-27 21:40:19 +0300

    serxere

---
+++ mimari/app/api/route.ts
@@ -0,0 +1,28 @@
+//  Bismillahirahmanirahim
+
+
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export  function POST(req:NextRequest, res:NextResponse) {
+
+    
+
+
+
+
+
+
+
+        
+console.log(req.body)
+    
+        
+    
+        
+
+    
+return res.json()
+
+
+}
